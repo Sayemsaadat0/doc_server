2f76e4fd13272ab27ace6706f43ebe9ab4bd4c39
Sayemsaadat0 <sayemsaadat0@gmail.com>
2023-10-05 20:19:22 +0600

    connected mongodb

--- index.js
+++ index.js
@@ -1,19 +1,48 @@
-const express = require('express');
-const cors = require('cors'); 
-const port = process.env.PORT || 5000
-
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
 
 const app = express();
 
-// middleware 
-app.use(cors()); 
+// middleware
+app.use(cors());
 app.use(express.json());
 
-app.get('/', async(req, res) => {
-    res.send('dOCTOR IS RUNNING');
-}); 
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.njebycd.mongodb.net/?retryWrites=true&w=majority`;
 
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    client.connect();
+    const appoinmentOptionsCOllection = client
+      .db("doc-portal")
+      .collection("appoinmentOptions");
+
+    app.get("/appoinmentOptions", async (req, res) => {
+        const query = {}
+      const result = await appoinmentOptionsCOllection.find(query).toArray();
+      res.send(result);
+    });
+
+    await client.db("admin").command({ ping: 1 });
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+app.get("/", async (req, res) => {
+  res.send("dOCTOR IS RUNNING");
+});
 
 app.listen(port, () => {
-    console.log('Doctor portal is running');
+  console.log("Doctor portal is running");
 });
