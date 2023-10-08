f868a10fa66162ed54552409440997c8fda37760
Sayemsaadat0 <sayemsaadat0@gmail.com>
2023-10-08 22:10:11 +0600

    having error

--- index.js
+++ index.js
@@ -28,19 +28,21 @@ async function run() {
       .collection("appoinmentOptions");
     const bookingsCOllection = client.db("doc-portal").collection("bookings");
 
-    // appoinment options 
+    // appoinment options
     app.get("/appoinmentOptions", async (req, res) => {
       const query = {};
       const result = await appoinmentOptionsCOllection.find(query).toArray();
       res.send(result);
     });
 
-    app.post('/bookings', async(req, res)=>{
-      const booking = req.body 
-      console.log(booking);
+    // booking post
+    app.post("/bookings", async (req, res) => {
+      const date = req.query.date;
+      console.log("date", date);
+      const booking = req.body;
       const result = await bookingsCOllection.insertOne(booking);
       res.send(result);
-    } )
+    });
 
     await client.db("admin").command({ ping: 1 });
   } finally {
