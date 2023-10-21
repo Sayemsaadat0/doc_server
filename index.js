2e96102583545bd0a285777bb4c1d496c4a06b09
Sayemsaadat0 <sayemsaadat0@gmail.com>
2023-10-21 22:53:04 +0600

    new commit

--- index.js
+++ index.js
@@ -31,13 +31,18 @@ async function run() {
     // appoinment options 
     // use aggregation to query multiple collections and then merge data  
     app.get("/appoinmentOptions", async (req, res) => {
+      // get the date 
       const date = req.query.date;
       const query = {};
       const options = await appoinmentOptionsCOllection.find(query).toArray(); 
+      // set the date as a apoinment date
       const bookingQuery = {appoinment_Date: date} 
+    // finding selected booked 
       const alreadyBooked = await bookingsCOllection.find(bookingQuery).toArray() 
       options.forEach(option =>{
         const optionBooked = alreadyBooked.filter(book =>book.Treatment == option.name)
+        const bookedSlots = optionBooked.map(book => book.slot)
+        const remainingSlots = option.slots.filter(slot => bookedSlots.includes(slot))
       console.log('optionBooked', optionBooked)
       }) 
     
